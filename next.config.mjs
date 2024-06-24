9b31658aed683c3b326bd9b04217e0bd301dc0bf
Kaustubhya Shukla <kaustubhyaksd@gmail.com>
2024-06-24 22:27:14 +0530

    ...

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,3 @@
-/* In the notes I have said that server actions have changed, but we might not face issues even if we go with the old code structure
-
-We have gone with the code taught in Next JS 13
-
-*/
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
